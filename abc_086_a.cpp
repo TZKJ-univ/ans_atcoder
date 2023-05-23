6e68226f6749d4f20d989202f4502b07d59a4daf
W-Zama <wzama4667@gmail.com>
2023-05-23 19:00:47 +0900

    commit

---
+++ abc_086_a.cpp
@@ -0,0 +1,17 @@
+/**
+*    programmer:  Zama
+*    created: 14.05.2023 20:45:43
+**/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	if (a*b % 2 == 0) cout << "Even" <<endl;
+	else cout << "Odd" << endl;
+
+	return 0;
+}
