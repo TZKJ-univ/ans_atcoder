a392eeadeb3b41b2f75dc645ea97f29b080810b7
W-Zama <wzama4667@gmail.com>
2023-05-31 12:16:37 +0900

    commit

---
+++ abs_practiceA.cpp
@@ -0,0 +1,17 @@
+/**
+*    programmer:  Zama
+*    created: 14.05.2023 20:40:34
+**/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int a, b, c;
+	string s;
+	cin >> a >> b >> c >> s;
+	cout << a+b+c << " " << s << endl;
+
+	return 0;
+}
