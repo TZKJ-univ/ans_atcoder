cb0068b817ffc8f9d7c87c62d3f64333ccf50bb8
W-Zama <wzama4667@gmail.com>
2023-11-17 22:55:22 +0900

    ファイルの構成を変更

---
+++ abc/328/c.cpp
@@ -0,0 +1,77 @@
+/**
+ * author: Zama
+ * created: 11.11.2023 20:53:56
+ **/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#if __has_include(<atcoder/all>)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
+
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+
+#define OVERLOAD_REP(_1, _2, _3, name, ...) name
+#define REP1(i, n) for (auto i = std::decay_t<decltype(n)>{}; (i) != (n); ++(i))
+#define REP2(i, l, r) for (auto i = (l); (i) != (r); ++(i))
+#define rep(...) OVERLOAD_REP(__VA_ARGS__, REP2, REP1)(__VA_ARGS__)
+#define all(...) std::begin(__VA_ARGS__), std::end(__VA_ARGS__)
+#define rall(...) std::rbegin(__VA_ARGS__), std::rend(__VA_ARGS__)
+
+#define pb push_back
+#define YESNO(bool) if(bool){cout<<"YES"<<"\n";}else{cout<<"NO"<<"\n";}
+#define yesno(bool) if(bool){cout<<"yes"<<"\n";}else{cout<<"no"<<"\n";}
+#define YesNo(bool) if(bool){cout<<"Yes"<<"\n";}else{cout<<"No"<<"\n";}
+
+template <typename T> inline void print(const vector<T>& v, string s = " "){rep(i, v.size()) cout << v[i] << (i != (ll)v.size() - 1 ? s : "\n");}
+template <typename T, typename S> inline void print(const pair<T, S>& p){cout << p.first << " " << p.second << "\n";}
+template <typename T> inline void print(const T& x) {cout << x << "\n";}
+template <typename T, typename S> inline void print(const vector<pair<T, S>>& v){for (auto&& p : v) print(p);}
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+int main()
+{
+    int N, Q;
+    cin >> N >> Q;
+    string S;
+    cin >> S;
+    vi l(Q), r(Q);
+    rep(i, Q) {
+        cin >> l[i] >> r[i];
+    }
+
+    vi f(N-1);
+    rep(i, N-1) {
+        if (S[i] == S[i+1]) f[i] = 1;
+    }
+
+    vi cum_f(N);
+
+    cum_f[0] = 0;
+    rep(i, N-1) {
+        cum_f[i+1] = cum_f[i] + f[i];
+    }
+
+    rep(i, Q) {
+        int a = l[i]-1, b = r[i]-1;
+        // print(a);
+        // print(b);
+        cout << cum_f[b] - cum_f[a] << endl;
+    }
+
+    return 0;
+}
