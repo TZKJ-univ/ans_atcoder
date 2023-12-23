bcea2002a8f2f49fc861744de0593bd4d15849d1
W-Zama <wzama4667@gmail.com>
2023-12-23 15:49:40 +0900

    add file

---
+++ typical90/063_2.cpp
@@ -0,0 +1,90 @@
+/**
+ * author: Zama
+ * created: 20.12.2023 21:41:05
+ **/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#  include <debug_print.hpp>
+#  define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#  define debug(...) (static_cast<void>(0))
+#endif
+
+#if __has_include(<atcoder/all>)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
+
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+
+#define OVERLOAD_REP(_1, _2, _3, name, ...) name
+#define REP1(i, n) for (auto i = std::decay_t<decltype(n)>{}; (i) != (n); ++(i))
+#define REP2(i, l, r) for (auto i = (l); (i) != (r); ++(i))
+#define rep(...) OVERLOAD_REP(__VA_ARGS__, REP2, REP1)(__VA_ARGS__)
+#define all(...) std::begin(__VA_ARGS__), std::end(__VA_ARGS__)
+#define rall(...) std::rbegin(__VA_ARGS__), std::rend(__VA_ARGS__)
+
+#define pb push_back
+#define YESNO(bool) if(bool){cout<<"YES"<<"\n";}else{cout<<"NO"<<"\n";}
+#define yesno(bool) if(bool){cout<<"yes"<<"\n";}else{cout<<"no"<<"\n";}
+#define YesNo(bool) if(bool){cout<<"Yes"<<"\n";}else{cout<<"No"<<"\n";}
+
+template <typename T> inline void print(const vector<T>& v, string s = " "){rep(i, v.size()) cout << v[i] << (i != (ll)v.size() - 1 ? s : "\n");}
+template <typename T, typename S> inline void print(const pair<T, S>& p){cout << p.first << " " << p.second << "\n";}
+template <typename T> inline void print(const T& x) {cout << x << "\n";}
+template <typename T, typename S> inline void print(const vector<pair<T, S>>& v){for (auto&& p : v) print(p);}
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+int main()
+{
+    int H, W;
+    cin >> H >> W;
+    vvi P(H, vi(W));
+    rep(i, H) rep(j, W) cin >> P[i][j];
+
+    int ans = -1;
+
+    for (int bit = 1; bit < (1 << H); ++bit) {
+        vi mp(80001);
+        rep(i, W) {
+            int f = 1; // その列が同じ数だけで構成されているかどうか
+            int obj = -1;
+            rep(j, H) {
+                if (!(bit & (1 << j))) continue;
+
+                if (obj == -1) {
+                    obj = P[j][i];
+                } else {
+                    if (obj != P[j][i]) f = 0;
+                }
+            }
+            if (f) {
+                mp[obj]++;
+            }
+        }
+        int cnt = 0;
+        rep(i, H) {
+            if (bit & (1 << i)) cnt++;
+        }
+        // debug(mp);
+        chmax(ans, *max_element(all(mp)) * cnt);
+    }
+    print(ans);
+
+    return 0;
+}
